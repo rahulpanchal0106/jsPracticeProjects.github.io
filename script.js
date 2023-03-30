ec6fdfa91d04d6b1c802f1565969b10c00f133b1
rahulpanchal0106 <210130111009@gecg28.ac.in>
2023-03-30 17:00:18 +0530

    Deleting the files

--- script.js
+++
@@ -1,8 +0,0 @@
-let uname;
-
-document.getElementById("button1").onclick =function(){
-    uname = document.getElementById("input").value;
-    console.log(uname);
-    document.getElementById("myLabel").innerHTML = "Hello " + uname;
-    document.getElementById("inpdiv").innerHTML=" ";
-}
